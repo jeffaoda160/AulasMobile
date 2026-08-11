7b2cd06724d2994564ce32617e850bfdc53901ed
jeffaoda160 <gabriel.gregolan.lima@escola.pr.gov.br>
2026-08-11 10:00:07 -0300

    Create ex05.js

---
+++ aula 2 /ex05.js
@@ -0,0 +1,13 @@
+function tripDec(x) {
+    return x * 3;
+}
+
+const tripExp = function(x) {
+    return x * 3;
+};
+
+const tripArr = x => x * 3;
+
+console.log(tripDec(5));
+console.log(tripExp(5));
+console.log(tripArr(5));
